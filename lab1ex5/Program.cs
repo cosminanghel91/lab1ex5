1e9ebe53d245e798b534b156656df1846f2f0b76
Cosmin Anghel <cosmin.anghel@metaplast.ro>
2024-02-19 15:46:52 +0200

    Add project files.

---
+++ lab1ex5/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+* Se citesc doua numere de la tastatura, x,y. Scrieti un program care va afisa cele doua numere in ordine crescatoare
+*/
+
+Console.WriteLine("Introduceti valoarea pentru primul numar:");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduceti valoarea pentru al doilea numar:");
+int y = int.Parse(Console.ReadLine());
+
+if (x < y)
+{
+    Console.WriteLine("Ordonare crescatoare: " + x + " , " + y);
+}
+else
+{
+    Console.WriteLine("Ordonare crescatoare: " + y + " , " + x);
+}
